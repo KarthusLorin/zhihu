bbed85e42bab260b4fa79df70ee9c89c08dc24a4
KarthusLorin <karthuslorin@gmail.com>
2019-07-19 22:11:12 +0800

    change schema

--- app/models/users.js
+++ app/models/users.js
@@ -15,6 +15,44 @@ const userSchema = new Schema({
         type: String,
         required: true,
         select: false
+    },
+    avatar_url: {
+        type: String
+    },
+    gender: {
+        type: String,
+        enum: ['male', 'female'],
+        default: 'male',
+        required: true
+    },
+    // 一句话介绍
+    headline: {
+        type: String
+    },
+    location: {
+        type: [{type: String}]
+    },
+    business: {
+        type: String
+    },
+    employments: {
+        type: [{
+            company: {
+                type: String
+            },
+            job: {
+                type: String
+            }
+        }]
+    },
+    educations: {
+        type: [{
+            school: {type: String},
+            major: {type: String},
+            diploma: {type: Number, enum: [1,2,3,4,5]},
+            entrance_year: {type: Number},
+            graduation_year: {type: Number}
+        }]
     }
 })
 
